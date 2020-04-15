e428d56e6c7c0e9c32e5468db94a25d82be199c7
vudinhthi <vudinhthi@gmail.com>
2020-04-15 08:34:38 +0700

    Update form Cookies delete button

--- frmCookies.Designer.cs
+++ frmCookies.Designer.cs
@@ -28,23 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frmCookies
             // 
             this.ClientSize = new System.Drawing.Size(824, 540);
-            this.Controls.Add(this.button1);
             this.Name = "frmCookies";
             this.Text = "FVN - Cookies";
             this.ResumeLayout(false);
@@ -53,6 +41,5 @@
 
         #endregion
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
     }
 }
